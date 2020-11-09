4970c1f7424dbeabeabda4b1b34b7b7f3062a8c6
Michael <wtfdic@hotmail.com>
2020-11-09 20:06:15 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(num)
-  if (num % 5 == 0) && (num % 3 == 0)
-    return "FizzBuzz"
-  if num % 3 == 0
-    return "Fizz"
-  if num % 5 == 0
-    return "Buzz"
-  else
-    return
+def fizzbuzz(int)
+  if int % 3 == 0
+    "Fizz"
   end
 end
