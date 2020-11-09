3347a4289753ceb86cf94258ba084b7be2325deb
Michael <wtfdic@hotmail.com>
2020-11-09 20:03:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
-
+def fizzbuzz(num)
+  if (num % 5 == 0) && (num % 3 ==0)
+    return "FizzBuzz"
+  if num % 3 == 0
+    return "Fizz"
+  if num % 5 == 0
+    return "Buzz"
+  else
+    return
+  end
 end
